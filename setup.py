11fbb63f83ff47737c17a922bc244f0fc6fc6092
Carsten Arnholm <arnholm@arnholm.org>
2022-04-12 15:11:55 +0200

    needs tk

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 setuptools.setup(   name='examplepackage',
-                    version='0.1',
+                    version='0.2',
                     description='An example package',
                     url='#',
                     author='max',
-                    install_requires=['opencv-python'],
+                    install_requires=['opencv-python','tk'],
                     author_email='',
                     packages=setuptools.find_packages(),
                     zip_safe=False)
